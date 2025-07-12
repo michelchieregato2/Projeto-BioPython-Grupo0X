2d8ab30f0eb86a8aec833ec53b2dffbace8c2bfe
Michel Chieregato <michel.chieregato@gmail.com>
2025-07-12 11:52:54 -0300

    Criando a OrganismoFasta

--- bio/organismo_fasta.py
+++ bio/organismo_fasta.py
@@ -1,4 +1,7 @@
 class OrganismoFasta:
-    """
-    Criar
-    """
+    
+    def __init__(self, id, nome, sequencia):
+        self.id = id
+        self.nome = nome
+        self.sequencia = sequencia
+
